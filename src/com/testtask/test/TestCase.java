57e75bae61f169d0bb611a3360cab0a88f1a1313
lvarendi <60733493+lvarendi@users.noreply.github.com>
2020-02-06 11:47:32 +0200

    Fix chromedriver location

--- src/com/testtask/test/TestCase.java
+++ src/com/testtask/test/TestCase.java
@@ -9,7 +9,7 @@ public class TestCase {
 	protected WebDriver driver;
 
 	public void init(final String url) {
-		System.setProperty("webdriver.chrome.driver", "..\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get(url);
 	}
